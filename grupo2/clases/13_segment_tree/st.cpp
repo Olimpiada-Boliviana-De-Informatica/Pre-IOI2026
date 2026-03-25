6ee6bbe0a4ef6c28826634eb19999e5148e3dda2
Diego Angulo Ramirez <diegoangulo5@gmail.com>
2026-03-25 19:32:20 -0400

    Added comments

--- grupo2/clases/13_segment_tree/st.cpp
+++ grupo2/clases/13_segment_tree/st.cpp
@@ -1,3 +1,6 @@
+// Enlace al problema: https://cses.fi/problemset/task/1648/
+// Tutorial de CP-Algorithms: https://cp-algorithms.com/data_structures/segment_tree.html
+
 #include <iostream>
 #include <vector>
 
@@ -8,6 +11,7 @@ vector<long long> st(4 * 200000 + 10);
 
 void init(int node, int l, int r) {
     if (l == r) {
+        // Estoy en el nodo hoja, o sea el nodo que representa un solo elemento del arreglo
         st[node] = arr[l];
         return;
     }
@@ -19,10 +23,11 @@ void init(int node, int l, int r) {
 }
 
 long long query(int node, int l, int r, int p, int q) {
-    if (l >= p && r <= q)
+    if (l >= p && r <= q)  // Estoy completamente dentro del rango de consulta
         return st[node];
-    if (l > q || r < p)
+    if (l > q || r < p)  // Estoy completamente fuera del rango de consulta
         return 0;
+    // Estoy parcialmente dentro del rango de consulta, tengo que consultar a mis hijos
     int izquierda = 2 * node + 1;
     int derecha = 2 * node + 2;
     long long consulta_l = query(izquierda, l, (l + r) / 2, p, q);
@@ -32,17 +37,24 @@ long long query(int node, int l, int r, int p, int q) {
 
 void update(int node, int l, int r, int pos, long long val) {
     if (l == r && l == pos) {
+        // Estoy en el nodo hoja que representa el elemento que quiero actualizar
         st[node] = val;
         arr[pos] = val;
         return;
     }
     if (l > pos || r < pos) {
+        // Estoy completamente fuera del rango del nodo, no tengo que actualizar nada, retorno algo
+        // neutro, que no afecte a la consulta, en este caso como la consulta es una suma, el neutro
+        // es el 0
         return;
     }
+    // Estoy dentro del rango del nodo, pero no soy el nodo hoja que representa el elemento que
+    // quiero actualizar, tengo que actualizar a mis hijos
     int izquierda = 2 * node + 1;
     int derecha = 2 * node + 2;
     update(izquierda, l, (l + r) / 2, pos, val);
     update(derecha, (l + r) / 2 + 1, r, pos, val);
+    // Mis hijos ya están actualizados, ahora tengo que actualizar mi valor
     st[node] = st[izquierda] + st[derecha];
 }
 
